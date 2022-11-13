5614dc08c9387b39c06ee6911cca94de7d8028df
cpr-jar <jan.reitzer@cpr-robots.com>
2022-11-13 17:30:48 +0100

    first commit

---
+++ CMakeHelloWorld/CMakeHelloWorld/CMakeHelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeHelloWorld.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "CMakeHelloWorld.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
